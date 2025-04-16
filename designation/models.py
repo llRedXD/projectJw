f43ae51833ff1b3d13e840a324cd6cf954f538c2
llRedXD <gh.o.neira@hotmail.com>
2025-04-16 17:59:52 -0300

    feat: :sparkles: adiciona modelos 'Designation' e 'Session' ao aplicativo 'designation'

--- designation/models.py
+++ designation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Session(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Designation(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, related_name="designations"
+    )
+
+    def __str__(self):
+        return self.name
