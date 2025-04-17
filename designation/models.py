b353d5fcabc57633647b4164cb48bb475439a4c9
llRedXD <gh.o.neira@hotmail.com>
2025-04-17 08:24:22 -0300

    refactor: :recycle: remove modelos e arquivos do aplicativo 'people' e 'reunion'

--- designation/models.py
+++ designation/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
-
-class Session(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-
-class Designation(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
-    session = models.ForeignKey(
-        Session, on_delete=models.CASCADE, related_name="designations"
-    )
-
-    def __str__(self):
-        return self.name
+# Create your models here.
